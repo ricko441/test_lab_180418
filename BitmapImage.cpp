5dad5c7002a50872325d590de310c53044689ffe
ricar <riccardo.frangini@stud.unifi.it>
2018-04-18 16:26:04 +0200

    Added save to JPEG

--- BitmapImage.cpp
+++ BitmapImage.cpp
@@ -16,6 +16,16 @@ bool BitmapImage::load(std::string name) {
 
 }
 
+bool BitmapImage::save(std::string name) {
+    // check it is a supported image file
+    std::string fileExt = name.substr(name.find_last_of(".") + 1);
+    if (fileExt == "jpg") {
+        // save image file
+        return true;
+    }
+    return false;
+}
+
 BitmapImage::~BitmapImage() {
     delete[] buffer;
 }
